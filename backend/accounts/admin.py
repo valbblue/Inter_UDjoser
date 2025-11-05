a43c5f137c8565766b3471394e5f1ce09cc5e605
Valentina Vergara <valentina.vergara09@inacapmail.cl>
2025-11-05 16:08:11 -0300

    Update admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 
 # CUENTAS CREADAS :D
 
-#Superusuario
-    #Username: admin
-    #Email: minyoonkook13@gmail.com
-    #Password: SIHU2025
-
 #Usuario Estudiante
   #{"email": "estudiante@inacapmail.cl",
   #"password": "Champurriado3000",
